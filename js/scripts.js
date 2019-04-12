0ce7db0acf7242e21b1769377360ac0ef23df2f4
Rocco Riccardi <rriccardi@vhtcx.com>
2019-04-12 15:49:20 -0400

    beefing up ui, refactored scripts

--- js/scripts.js
+++ js/scripts.js
@@ -32,9 +32,54 @@ function execute(input) {
         output = 'Kim Kardashian'
         certainty = Math.floor(results.kardashian * 100)
     }
+	displayResult(output, certainty);
 
     return "I'm " + certainty + "% sure that tweet was written by " + output;
 }
 
+function displayResult(output, certainty) {
+	if (certainty <= 60) {
+		displayTossUp(output, certainty);
+	}
+	if (output === 'Donald Trump') {
+		displayTrump(output, certainty);
+	}
+	else {
+		displayKim(output, certainty);
+	}
+}
+
+function displayTossUp(output, certainty) {
+	writeResult('tossUpText', output, certainty)
+	show('tossUpResult');
+	show('tossUpText');
+}
+
+function displayTrump(output, certainty) {
+	writeResult('trumpText', output, certainty)
+	show('trumpResult');
+	show('trumpText');
+}
+
+function displayKim(output, certainty) {
+	writeResult('kimKText', output, certainty)
+	show('kimKResult');
+	show('kimKText');
+}
+
+function writeResult(name, output, certainty) {
+	let text = "<h5>I'm " + certainty + "% sure that tweet was written by " + output + "</h5>";
+	document.getElementById(name).innerHTML = text;
+}
+
+function show(name) {
+	document.getElementById(name).style.display = 'block';
+}
+
+function hideAll() {
+	document.getElementById('tossUpResult').style.display = 'none';
+	document.getElementById('trumpResult').style.display = 'none';
+	document.getElementById('kimKResult').style.display = 'none';
+}
+
 train(trainingData);
-console.log(execute("Paste your tweet here"));
